40de781188ade8f8630c4627ca1078bf6eb168a2
Alvin KN <alvinkn.dev@gmail.com>
2023-03-30 09:37:20 +0700

    init project

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header>
+      <img
+        src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"
+        className="cursor-pointer object-contain"
+        height={100}
+        width={100}
+      />
+      <div>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+export default Header;
